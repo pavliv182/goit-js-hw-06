7014e3e1eabdb83884c9e3583bbf54b3be1134ad
pavliv182 <uptn182@gmail.com>
2022-07-10 12:23:28 +0300

    const allBoxes = document.createDocumentFragment;

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
 // 1. Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
 
 // 2. Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
@@ -12,13 +6,14 @@
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 // Все элементы должены иметь случайный цвет фона в формате HEX.
 // Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 
 // 3. Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 const refs = {
   inputEl: document.querySelector("input"),
   createBtn: document.querySelector("[data-create]"),
@@ -28,12 +23,35 @@ const refs = {
 // console.log(refs.createBtn);
 // console.log(refs.destroyBtn);
 
+// Создаём номинальный размер коробки для дальнейших правок
+let boxSize = 20;
+// Создаём "ёмкость" для всех коробок, которые мы создадим
+const allBoxes = document.createDocumentFragment;
+
 refs.createBtn.addEventListener("click", onClickCreate);
 
 function onClickCreate(event) {
   const value = refs.inputEl.value;
+  createBoxes(value);
   console.dir(value);
 }
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    boxSize += 10;
+
+    div.style.background = getRandomHexColor();
+    div.style.width = boxSize + "px";
+    div.style.height = boxSize + "px";
+    // div.style.width = `${boxSize}px`;
+    // div.style.height = `${boxSize}px`;
+
+    // console.log(div.style.width);
+    // console.log(div.style.height);
+  }
+}
+
 // const refs = {
 //   boxesEl: document.querySelector('#boxes'),
 //   createBtn: document.querySelector('button[data-create]'),
